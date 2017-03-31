dfa65ffdbbfa64e85a580ed472b1f8612515e8db
Lyndon Maydwell <lyndon@sordina.net>
2017-03-31 20:09:02 +1100

    options development

--- src/Network/Wai/Middleware/Options.hs
+++ src/Network/Wai/Middleware/Options.hs
@@ -12,45 +12,42 @@ module Network.Wai.Middleware.Options where
 import Servant
 import Servant.Foreign
 import Network.Wai
-import Data.Text
+import Data.Text hiding (null, zipWith)
 import Network.HTTP.Types.Method
-
-{- listFromAPI :: forall k (lang :: k) ftype api.
-   (HasForeign lang ftype api, GenerateList ftype (Foreign ftype api))
-=> Proxy lang -> Proxy ftype -> Proxy api -> [Req ftype]
-
-  data Req f
-  = Req {_reqUrl        :: Url f,
-         _reqMethod     :: Method,
-         _reqHeaders    :: [HeaderArg f],
-         _reqBody       :: Maybe f,
-         _reqReturnType :: Maybe f,
-         _reqFuncName   :: FunctionName} -}
+import Data.Maybe
 
 provideOptions :: (GenerateList NoContent (Foreign NoContent api), HasForeign NoTypes NoContent api)
-               => Proxy api -> (Request -> t -> IO b) -> Request -> t -> IO b
+               => Proxy api -> Middleware
 provideOptions apiproxy app req res = do
   print req
   print rmeth
   print pinfo
   print mlist
   if rmeth == "OPTIONS"
-     then opts
-     else app req res
+     then optional prior pinfo mlist
+     else prior
   where
   rmeth = requestMethod req :: Method
   pinfo = pathInfo      req :: [ Text ]
-  mlist = listFromAPI (Proxy :: Proxy NoTypes) (Proxy :: Proxy NoContent) apiproxy -- :: [Req NoContent]
-  opts  = undefined
+  mlist = listFromAPI (Proxy :: Proxy NoTypes) (Proxy :: Proxy NoContent) apiproxy
+  prior = app req res
 
-{-
+optional :: IO ResponseReceived -> [Text] -> [Req NoContent] -> IO ResponseReceived
+optional prior ts rs = if null methods
+                          then prior
+                          else optionsResponse methods
+  where
+  methods = mapMaybe (getMethod ts) rs
 
-constructPath =
-Req { _reqUrl        = Url {_path = [Segment {unSegment = Static (PathSegment {unPathSegment = "signup"})}], _queryStr = []},
-      _reqMethod     = "POST",
-      _reqHeaders    = [],
-      _reqBody       = Nothing,
-      _reqReturnType = Just NoContent,
-      _reqFuncName   = FunctionName {unFunctionName = ["post","signup"]}}
+getMethod :: [Text] -> Req NoContent -> Maybe Method
+getMethod rs ps = if and $ zipWith matchSegment rs (_path $ _reqUrl ps)
+                     then Just (_reqMethod ps)
+                     else Nothing
 
--}
+matchSegment :: Text -> Segment NoContent -> Bool
+matchSegment _ ( Segment (Cap _) )                              = True
+matchSegment a ( Segment (Static (PathSegment b)) ) | a == b    = True
+                                                    | otherwise = False
+
+optionsResponse :: [Method] -> IO ResponseReceived
+optionsResponse = undefined
