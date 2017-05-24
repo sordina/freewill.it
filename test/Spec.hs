d52c2a5c54b1f91fe4815df4c70484d7b563f8cd
Lyndon Maydwell <lyndon@sordina.net>
2017-05-24 16:32:05 +1000

    log level option

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,8 +8,7 @@ failEither m (Left  e) = print "Test Failed" >> print m >> print e >> exitFailur
 failEither _ (Right _) = return ()
 
 main :: IO ()
-main = do
-  failEither "Testing MemDB Sanity Check" =<< MemDB.test
+main = failEither "Testing MemDB Sanity Check" =<< MemDB.test
 
 -- TODO: Comparative tests with DB
 -- TODO: Simple error properties for double-decide, etc.
