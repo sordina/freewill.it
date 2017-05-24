716091b2c5c9a007f69c865e37e07c268f8255fb
Lyndon Maydwell <lyndon@sordina.net>
2017-05-24 16:25:44 +1000

    log level option

--- app/Main.hs
+++ app/Main.hs
@@ -9,25 +9,30 @@ import Network.Wai.Handler.Warp
 import Options.Generic
 import Data.Maybe
 
-import qualified Enhancements  as E
-import qualified API           as A
-import qualified DB.MemDB      as MemDB
-import qualified DB.PostgresDB as PostgresDB
-import qualified Servant.JS    as SJ
-import qualified Context       as C
+import qualified Enhancements                         as E
+import qualified API                                  as A
+import qualified DB.MemDB                             as MemDB
+import qualified DB.PostgresDB                        as PostgresDB
+import qualified Servant.JS                           as SJ
+import qualified Context                              as C
+import qualified Network.Wai                          as W
+import qualified Network.Wai.Middleware.RequestLogger as WL
+import qualified Network.Wai.Middleware.Debugging     as WL
 
-data Database = Memory | Postgres
-  deriving (Eq, Show, Read, Generic)
+data Database = Memory | Postgres    deriving (Eq, Show, Read, Generic)
+data LogLevel = Prod   | Dev | Debug deriving (Eq, Show, Read, Generic)
 
 data Options = Options { port     :: Maybe Int
                        , database :: Maybe Database <?> "Memory | Postgres (Default)"
                        , jwtKey   :: Maybe FilePath <?> "JWT Key FilePath"
                        , safeAuth :: Maybe Bool     <?> "False | True (Default) - Mandate HTTPS for Auth"
                        , jsURL    :: Maybe Text     <?> "URL that Javascript points to"
+                       , logLevel :: Maybe LogLevel <?> "Prod | Dev (Default) | Debug"
                        }
   deriving (Show, Generic)
 
 instance ParseField  Database
+instance ParseField  LogLevel
 instance ParseRecord Options
 
 main :: IO ()
@@ -38,12 +43,14 @@ main = do
       oJSU  = fromMaybe ""       $ unHelpful $ jsURL    opts
       oJKO  =                      unHelpful $ jwtKey   opts
       oSAO  =                      unHelpful $ safeAuth opts
+      oLog' = fromMaybe Dev      $ unHelpful $ logLevel opts
       oJSO  = SJ.defCommonGeneratorOptions { SJ.urlPrefix = oJSU }
+      oLog  = getLogger oLog'
   ctx      <- C.getContext oJKO oSAO
 
   putStrLn $ "Using " ++ show oDB ++ " database driver"
   putStrLn $ "Running on http://localhost:" ++ show oPort ++ "/"
-  go ctx oPort oJSO oDB
+  go ctx oPort oJSO oLog oDB
 
 newMemDBConnection :: IO (MemDB.MemDBConnection (A.M a))
 newMemDBConnection = MemDB.newMemDBConnection
@@ -51,12 +58,17 @@ newMemDBConnection = MemDB.newMemDBConnection
 newPostgresDBConnection :: IO (PostgresDB.PostgresConnection (A.M a))
 newPostgresDBConnection = PostgresDB.newPostgresDBConnection
 
-go :: C.CTX -> Int -> SJ.CommonGeneratorOptions -> Database -> IO ()
+getLogger :: LogLevel -> W.Middleware
+getLogger Prod  = WL.logStdout
+getLogger Dev   = WL.logStdoutDev
+getLogger Debug = WL.debug . WL.logStdoutDev
 
-go c p jso Memory = do
+go :: C.CTX -> Int -> SJ.CommonGeneratorOptions -> W.Middleware -> Database -> IO ()
+
+go c p jso logger Memory = do
   db <- newMemDBConnection
-  run p (E.app c db jso)
+  run p (E.app c db jso logger)
 
-go c p jso Postgres = do
+go c p jso logger Postgres = do
   db <- newPostgresDBConnection
-  run p (E.app c db jso)
+  run p (E.app c db jso logger)
--- src/Enhancements.hs
+++ src/Enhancements.hs
@@ -17,8 +17,8 @@ import Servant.Auth.Server
 import Servant.JS hiding (requestBody)
 import Data.Text
 import Data.Swagger (Swagger(..) )
+import Network.Wai
 import Network.Wai.Middleware.Cors
-import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Servant.Options
 -- import qualified Network.Wai.Middleware.Debugging as L
 
@@ -33,11 +33,12 @@ type App       = VanillaJS
             :<|> Redirect "index.html"
 
 app :: (HasContextEntry x JWTSettings, HasContextEntry x CookieSettings, Database db M)
-    => Context x -> db -> CommonGeneratorOptions -> Application
-app context db jsOptions = logStdoutDev
-                   $ cors (const $ Just policy) -- simpleCors
-                   $ provideOptions api
-                   $ serveWithContext apiWithEnhancements context (serverWithSpec db js jsOptions cs)
+    => Context x -> db -> CommonGeneratorOptions -> Middleware -> Application
+app context db jsOptions logging
+    = logging
+    $ cors (const $ Just policy) -- simpleCors
+    $ provideOptions api
+    $ serveWithContext apiWithEnhancements context (serverWithSpec db js jsOptions cs)
   where
   js     = getContextEntry context :: JWTSettings
   cs     = getContextEntry context :: CookieSettings
