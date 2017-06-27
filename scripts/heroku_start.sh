b911c7f25737a1757124cb267dc98a5f171a7995
Lyndon Maydwell <lyndon@sordina.net>
2017-06-27 23:57:14 +1000

    key

--- scripts/heroku_start.sh
+++ scripts/heroku_start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 if [ "" != "$JWT_KEY" ]
 then
 	echo "Using JWT Key from Env variable JWT_KEY"
 	echo "$JWT_KEY" > /app/key.jwk
+	echo "Key set."
 fi
 
 /app/vendor/heroku_binaries/latest_heroku_binary --database "$DATABASE_URL" --safeAuth True --jwtKey /app/key.jwk --logLevel Prod --port $PORT
