eabccf549cccd6591bf9126d83583048e87e4710
Lyndon Maydwell <lyndon@sordina.net>
2017-06-28 12:15:22 +1000

    minify js on heroku

--- scripts/heroku_start.sh
+++ scripts/heroku_start.sh
@@ -5,8 +5,8 @@ set -e
 if [ "" != "$JWT_KEY" ]
 then
 	echo Using JWT_KEY from env
-	/app/vendor/heroku_binaries/latest_heroku_binary --database "$DATABASE_URL" --safeAuth True --jwtKey <(echo "$JWT_KEY") --logLevel Prod --port $PORT
+	/app/vendor/heroku_binaries/latest_heroku_binary --database "$DATABASE_URL" --safeAuth True --jwtKey <(echo "$JWT_KEY") --logLevel Prod --port $PORT --minified
 else
 	echo Using random JWT_KEY
-	/app/vendor/heroku_binaries/latest_heroku_binary --database "$DATABASE_URL" --safeAuth True --logLevel Prod --port $PORT
+	/app/vendor/heroku_binaries/latest_heroku_binary --database "$DATABASE_URL" --safeAuth True --logLevel Prod --port $PORT --minified
 fi
